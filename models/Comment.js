47226e480627856fc30181faf980c4cfebabfb1f
qgtere <86640621+qgtere@users.noreply.github.com>
2022-10-04 17:47:01 -0600

    comment model added

---
+++ models/Comment.js
@@ -0,0 +1,36 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'post',
+                key: 'id',
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    }
+);
+
+module.exports = Comment;
